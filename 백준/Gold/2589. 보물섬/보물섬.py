500a452da9cd8fbbd9478d1e41d39168c395a36f
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2023-03-08 20:14:30 +0900

    [Gold V] Title: 보물섬, Time: 768 ms, Memory: 122544 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -8,15 +8,13 @@ max_dist = 0 #모든 좌표를 시작점으로 하여 최단거리로 돌면서 
 
 def bfs(i, j):
     global max_dist
-    visited = [[False for _ in range(C)] for _ in range(R)]
-    dist = [[0 for _ in range(C)] for _ in range(R)]
-
     q = deque()
-    q.append([i, j])
+    visited = [[False for _ in range(C)] for _ in range(R)]
+    q.append([i, j, 0])
     visited[i][j] = True
 
     while q:
-        r, c = q.popleft()
+        r, c, dist = q.popleft()
         for d in dir:
             nr = r + d[0]
             nc = c + d[1]
@@ -24,14 +22,13 @@ def bfs(i, j):
                 continue
             if visited[nr][nc] or arr[nr][nc] == "W":
                 continue
-            q.append([nr, nc])
-            dist[nr][nc] = dist[r][c] + 1
+            q.append([nr, nc, dist+1])
             visited[nr][nc] = True
-        max_dist = max(max_dist, dist[r][c])
-#
-# #bfs에서 맨 마지막으로 나오는 원소는 맨 마지막 너비 == 가장 멀리 있는 좌표가 나오는 것 이용
-# #각 bfs가 visited를 공유하면 안됨
-#
+        max_dist = max(max_dist, dist)
+
+#bfs에서 맨 마지막으로 나오는 원소는 맨 마지막 너비 == 가장 멀리 있는 좌표가 나오는 것 이용
+#각 bfs가 visited를 공유하면 안됨
+
 for i in range(R):
     for j in range(C):
         if arr[i][j] == "L":
