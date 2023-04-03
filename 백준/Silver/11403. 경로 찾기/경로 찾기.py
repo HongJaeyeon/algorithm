dbe9eeef4bfe8fee3d811527b5372f3e9b87222d
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2023-04-04 00:03:41 +0900

    [Silver I] Title: 경로 찾기, Time: 232 ms, Memory: 116832 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,25 @@
+import sys
+n = int(input())
+ls = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+dist = [[float('inf') for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if ls[i][j] == 1:
+            dist[i][j] = 1
+
+
+for path in range(n):
+    for i in range(n):
+        for j in range(n):
+            if dist[i][j] > dist[i][path] + dist[path][j]:
+                dist[i][j] = dist[i][path] + dist[path][j]
+
+for r in dist:
+    for ele in r:
+        if ele != float('inf'):
+            print(1, "", end="")
+        else:
+            print(0, "", end="")
+    print()
