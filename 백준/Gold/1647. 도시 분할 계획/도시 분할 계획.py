4f399f912dcf3f513a0e987a1b472ba323ab973e
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2023-06-29 21:06:27 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2460 ms, Memory: 278164 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,38 +1,39 @@
 import sys
-input = sys.stdin.readline
-V, E = map(int, input().strip().split())
-parents = [0 for _ in range(V+1)]
 
-def makeSet():
-    for i in range(1, V):
+def makeSet(): #자기가 대표인 집합 만들기
+    for i in range(1, V+1):
         parents[i] = i
 
-def findParent(i):
-    if parents[i] == i:
-        return i
-    parents[i] = findParent(parents[i])
-    return parents[i]
+def findParent(a):
+    if parents[a] == a:
+        return a
+    parents[a] = findParent(parents[a])
+    return parents[a]
 
-def union(i, j):
-    i = findParent(i)
-    j = findParent(j)
-    if i == j: return False
-    parents[j] = i
+def union(a, b):
+    pa = findParent(a)
+    pb = findParent(b)
+    # 부모가 같다면 합칠 수 없음 (사이클 존재)
+    if pa == pb:
+        return False
+    parents[pb] = pa
     return True
 
-edges = []
-for _ in range(E):
-   edges.append(list(map(int, input().strip().split())))
-edges.sort(key=lambda x: x[2])
+V, E = map(int, sys.stdin.readline().rstrip().split())
+edges = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(E)]
+parents = [0 for _ in range(V+1)]
 
+edge_cnt = 0
+edges.sort(key=lambda x: x[2])
 ans = 0
-cnt = 0
+
 makeSet()
-for edge in edges:
-    s, t, w = edge
-    if union(s, t):
-        ans += w
-        cnt += 1
-    if cnt == V - 2:
+for s, t, w in edges:
+    if not union(s, t):
+        continue
+    if edge_cnt == V-2:
         break
+    ans += w
+    edge_cnt += 1
+
 print(ans)
