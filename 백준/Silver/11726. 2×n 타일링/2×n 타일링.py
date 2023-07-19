1eb5445cb48b0378f8d25e0857d80bedf2eefaf6
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2023-07-19 21:03:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 114644 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,38 +1,43 @@
-# def rec(k): # 길이가 k인 그거를 채우는 경우의수
-#     global ans
-#     if k < 0: #
+# import sys
+# sys.stdin = open("res/tc.txt")
+#
+# n = int(input())
+# cnt = 0
+# def recur(leftLength):
+#     global cnt
+#     if leftLength == 0:
 #         return
-#     if k == 0:
-#         ans += 1
+#     if leftLength == 2:
+#         cnt += 2
 #         return
-#     rec(k - 1)
-#     rec(k - 2)
-
-# def rec(k): # 길이가 k인 그거를 채우는 경우의수
-#     if k < 0:
-#         return 0
-#     if k == 0:
-#         return 1
-#     return rec(k - 1) + rec(k - 2)
-
-def rec(k): # 길이가 k인 그거를 채우는 경우의수
-    if k < 0:
-        return 0
-    if k == 0:
-        return 1
-    if dp[k] != -1: # 메모이제이션
-        return dp[k]
-    dp[k] = (rec(k - 1) + rec(k - 2)) % 10007
-    return dp[k]
-
-
-
+#     if leftLength == 1:
+#         cnt += 1
+#         return
+#
+#     recur(leftLength-1)
+#     recur(leftLength-2)
+#
+# recur(n)
+# print(cnt)
+import sys
 n = int(input())
-# ans = 0
-# rec(n)
-# print(ans)
 
-# print(rec(n))
+# 남은 길이가 i일 때를 다시 계산하지 않게. dp[i]에 저장해둔다.
+lhs = [-1 for _ in range(n+1)]
 
-dp = [-1] * (n + 1)
-print(rec(n))
+
+def recur(leftLength):
+    if leftLength == 0:
+        return 0
+    if leftLength == 2:
+        return 2
+    if leftLength == 1:
+        return 1
+    if lhs[leftLength] != -1:
+        return lhs[leftLength]
+
+    #내가 선택했을 때 남은 녀석들의 가지수
+    lhs[leftLength] = (recur(leftLength-1) + recur(leftLength-2)) % 10007
+    return lhs[leftLength]
+
+print(recur(n))
