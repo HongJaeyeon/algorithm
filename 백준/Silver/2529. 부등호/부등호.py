123b8b06df9eefe4e7e0572962625a692fc91547
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2023-10-18 20:50:28 +0900

    [Silver I] Title: 부등호, Time: 184 ms, Memory: 112364 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,42 +1,39 @@
 import sys
+lambda input: sys.stdin.readline().strip()
 
-n = int(input())
-input = lambda: sys.stdin.readline().strip().split()
-ls = list(input())
-selected = [-1 for _ in range(n+1)]
-visited = [False for _ in range(10)]
+N = int(input())
+ls = list(input().split())
+selected = [0 for _ in range(N+1)]
+visited = [0 for _ in range(10)]
+
 maxValue = str(-float('inf'))
 minValue = str(float('inf'))
-def recur(depth, selected, visited):
+
+def recur(depth):
     global maxValue, minValue
     if depth >= 2:
-        if ls[depth-2] == "<" and selected[depth-2] >= selected[depth-1]:
-            return
+        if ls[depth-2] == "<":
+            if selected[depth-2] > selected[depth-1]:
+                return
+        else:
+            if selected[depth - 2] < selected[depth - 1]:
+                return
 
-        if ls[depth-2] == ">" and selected[depth-2] <= selected[depth-1]:
-            return
-
-    if depth == n+1:
-        maxValue = max(maxValue, ''.join(map(str, selected)))
-        minValue = min(minValue, ''.join(map(str, selected)))
+    if depth == N+1: #뽑는 개수
+        tmp = ''.join(map(str, selected))
+        maxValue = max(maxValue, tmp)
+        minValue = min(minValue, tmp)
         return
-
-    # for i in range(10):
-    #     selected[depth] = i
-    #     recur(depth+1, selected)
-
-    #선택된 숫자는 모두 달라야하므로
-    # for i in range(start, 10):
-    #     selected[depth] = i
-    #     recur(depth+1, selected, i+1)
 
     for i in range(10):
         if visited[i]:
             continue
         visited[i] = True
         selected[depth] = i
-        recur(depth+1, selected, visited)
+        recur(depth+1)
         visited[i] = False
-recur(0, selected, visited)
+recur(0)
+
 print(maxValue)
 print(minValue)
+
