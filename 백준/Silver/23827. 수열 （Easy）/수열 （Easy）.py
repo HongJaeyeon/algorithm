f8f02d6c66684712ebf156b0f0e297fb4710ef5c
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2023-11-22 14:40:08 +0900

    [Silver IV] Title: 수열 (Easy), Time: 316 ms, Memory: 199252 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.py
@@ -0,0 +1,15 @@
+import sys
+lambda input : sys.stdin.readline().strip()
+
+n = int(input())
+ls = list(map(int, input().split()))
+ans = 0
+
+ls.sort()
+sumValue = sum(ls)
+
+for ele in ls:
+    sumValue -= ele
+    ans += ele * sumValue
+
+print(ans % 1000000007)
