3179fff5275ecc46286badca123ed4ad30afd96b
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2024-01-06 22:28:45 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 408 ms, Memory: 131252 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,46 +1,33 @@
 import sys
 
-def makeset():
-    global arr, v, e, parents
-    for i in range(v+1):
-        parents[i] = i;
+input = lambda : sys.stdin.readline().strip().split()
+v, e = map(int, input())
+ls = [list(map(int, input())) for _ in range(e)]
+# par = [i for i in range(1, v+1)]
 
-def findparent(i):
-    global arr, v, e, parents
-    if parents[i] == i:
-        return i
+par = [i for i in range(v+1)]
 
-    #재귀, 경로 압축
-    parents[i] = findparent(parents[i])
-    return parents[i]
+def findp(a):
+    if par[a] == a:
+        return a
+    par[a] = findp(par[a])
+    return par[a]
 
-def union(i,j):
-    global arr, v, e, parents
-
-    #대표 찾기
-    i = findparent(i)
-    j = findparent(j)
-
-    if i == j:
+def union(a, b):
+    a = findp(a)
+    b = findp(b)
+    if a == b:
         return False
-
-    parents[j] = i
+    par[a] = b
     return True
 
-v, e = map(int,sys.stdin.readline().strip().split())
-arr = [list(map(int,sys.stdin.readline().strip().split())) for _ in range(e)]
-parents = [0 for _ in range(v+1)]
-total = 0
-cnt = 0
+def kruskal():
+    weight = 0
+    ls.sort(key=lambda x: x[2])
+    for a, b, w in ls:
+        if union(a, b):
+            weight += w
 
-#무게에 대해서 정렬
-arr.sort(key= lambda x : x[2])
-makeset()
+    print(weight)
 
-for start, to, weight in arr:
-    if union(start, to) :
-        total += weight
-        cnt += 1
-        if cnt == v-1 : break
-
-print(total)
+kruskal()
