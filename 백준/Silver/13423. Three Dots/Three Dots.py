89efcc25030cf37e49ce67cd8b49239eac1650db
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2024-01-24 02:05:59 +0900

    [Silver II] Title: Three Dots, Time: 2892 ms, Memory: 112420 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.py
+++ 백준/Silver/13423. Three Dots/Three Dots.py
@@ -3,18 +3,17 @@ input = lambda : sys.stdin.readline().strip()
 tc = int(input())
 for _ in range(tc):
     N = int(input())
+    
     ls = list(map(int, input().split()))
-    st = set()
-    for ele in ls:
-        st.add(ele)
     ls.sort()
-    s = 0
-    e = N-1
     ans = 0
+    st = set(ls)
+
     for s in range(N):
         for e in range(N):
             if s == e:
                 continue
             if (ls[s] + ls[e]) / 2 in st:
                 ans += 1
+                
     print(ans//2)
