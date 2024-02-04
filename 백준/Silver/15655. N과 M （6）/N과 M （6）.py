ba32f99bed934361bd18ab20d214b9418551215c
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2024-02-04 18:15:50 +0900

    [Silver III] Title: N과 M (6), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,18 +1,20 @@
 import sys
-input = lambda: sys.stdin.readline().strip()
-N, M = map(int, input().split())
-ls = list(map(int, input().split()))
-ls.sort()
-selected = []
+input = lambda: sys.stdin.readline().strip().split()
 
-def recur(idx, cnt):
-    if idx == N: #모든 들어 올 수 있는 수를 다보고
-        if cnt == M: #뽑은 자리수가 M과 같다면
+n, c = map(int, input())
+ls = list(map(int, input()))
+selected = []
+ls.sort()
+
+def recur(depth, cnt):
+    if depth == n:
+        if cnt == c:
             print(*selected)
         return
-    selected.append(ls[idx])
-    recur(idx+1, cnt+1)
+
+    selected.append(ls[depth])
+    recur(depth+1, cnt+1)
     selected.pop()
-    recur(idx+1, cnt)
+    recur(depth+1, cnt)
 
 recur(0, 0)
