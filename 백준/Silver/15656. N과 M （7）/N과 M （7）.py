ee3eaf7958a3a28b2d3edc22dde9ef17ad40a496
홍재연 <39540473+HongJaeyeon@users.noreply.github.com>
2024-02-04 18:23:05 +0900

    [Silver III] Title: N과 M (7), Time: 876 ms, Memory: 128276 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,22 +1,19 @@
 import sys
+input = lambda: sys.stdin.readline().strip().split()
 
-input = lambda: sys.stdin.readline().strip()
-
-n, r = map(int, input().split())
-ls = list(map(int, input().split()))
+n, c = map(int, input())
+ls = list(map(int, input()))
+selected = [0 for _ in range(c)]
 ls.sort()
 
-visited = [0 for _ in range(n)] #index를 다룬다
-selected = [0 for _ in range(r)]
-
-def perm(depth):
-    if depth == r:
-        for i in selected:
-            print(ls[i], end=" ")
-        print()
+# 중복 순열
+def recur(depth):
+    if depth == c:
+        print(*selected)
         return
 
     for i in range(n):
-        selected[depth] = i
-        perm(depth+1)
-perm(0)
+        selected[depth] = ls[i]
+        recur(depth+1)
+
+recur(0)
